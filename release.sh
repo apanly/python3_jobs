dc08d70d4dc51452a4b63ad0d950ce95663909eb
vincent <apanly@163.com>
2020-10-14 10:30:36 +0800

    guowei  — 脚本修改

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sync(){
-    cmd="cd /data/www/private_deploy/python3_jobs  ;  git fetch --all ; git reset --hard origin/master"
+    cmd="cd /data/www/private_deploy/jobs  ;  git fetch --all ; git reset --hard origin/master"
     echo "\033[32m"$cmd
     eval $cmd
     DATE=`date +%Y%m%d%H%M%S`
@@ -10,21 +10,21 @@ sync(){
 }
 
 reload() {
-    cmd="export ops_config='hsh' && /data/www/python3/bin/uwsgi --reload /data/www/logs/python3_jobs/app.pid"
+    cmd="export ops_config='hsh' && /data/www/vir_python3/bin/uwsgi --reload /data/www/logs/python3_jobs/app.pid"
     echo "\033[32m"$cmd
     eval $cmd
     return 0
 }
 
 stop() {
-    cmd="export ops_config='hsh' && /data/www/python3/bin/uwsgi --stop /data/www/logs/python3_jobs/app.pid"
+    cmd="export ops_config='hsh' && /data/www/vir_python3/bin/uwsgi --stop /data/www/logs/python3_jobs/app.pid"
     echo "\033[32m"$cmd
     eval $cmd
     return 0
 }
 
 start() {
-    cmd="export ops_config='hsh' && /data/www/python3/bin/uwsgi --ini /data/www/private_deploy/python3_jobs/uwsgi.ini"
+    cmd="export ops_config='hsh' && /data/www/vir_python3/bin/uwsgi --ini /data/www/private_deploy/python3_jobs/uwsgi.ini"
     echo "\033[32m"$cmd
     eval $cmd
     return 0
