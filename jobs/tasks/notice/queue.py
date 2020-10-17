011f5ad639ddbf392e8f5a63da5e3b0641f2872c
vincent <apanly@163.com>
2020-10-17 13:58:59 +0800

    guowei  — 有一类报警先不用报警

--- jobs/tasks/notice/queue.py
+++ jobs/tasks/notice/queue.py
@@ -42,15 +42,17 @@ class JobTask( BaseJob ):
             "Job异常报警"
         ]
         for item in list:
+
             tmp_data = ModelHelper.model2Dict( item )
             tmp_job_info = ModelHelper.model2Dict(job_map.get(tmp_data['job_id']))
+            done_ids.append(tmp_data['id'])
             tmp_msg = "Job Id : {0},名称：{1},报警内容：{2}".format( tmp_job_info['id'],tmp_job_info['name'],tmp_data['content'] )
             if 'Job平台标识没有运行'  in tmp_msg or 'Job平台标识正在运行' not in tmp_msg:
                 continue
 
             self.handleItem(tmp_data, tmp_job_info)
             alert_content.append(tmp_msg)
-            done_ids.append( tmp_data['id'] )
+
 
         if done_ids:
             '''
