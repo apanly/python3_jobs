51172873bec68a732cf6899d4ce194aede9b3880
vincent <apanly@163.com>
2020-12-08 23:34:03 +0800

    guowei  —  这样好像就可以了

--- config/local_setting_demo.py
+++ config/local_setting_demo.py
@@ -10,3 +10,10 @@ SQLALCHEMY_TRACK_MODIFICATIONS=False
 SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/flask_framework?charset=utf8mb4'
 SQLALCHEMY_ENCODING = "utf8mb4"
 
+## 域名配置
+DOMAIN = {
+    "www": "http://127.0.0.1:" + str( SERVER_PORT ),
+    "resource" : "http://127.0.0.1:" + str( SERVER_PORT ) + "/static/cdn"
+}
+
+
